b7c97420cf4e99ca3850a1f44d88b2f975206787
Shahar Tal <chapo12345@gmail.com>
2022-11-13 19:07:03 +0200

    modified name print

--- GITrepo-ex3-shahar/Shahar.java
+++ GITrepo-ex3-shahar/Shahar.java
@@ -1,7 +1,7 @@
 public class Shahar {
   public static void main(String[] args) {
     for(int i=1; i <= 5; i++) {
-      System.out.println("Shahar");
+      System.out.println("Shahar.");
     }
   }
 }
